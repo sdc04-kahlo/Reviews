233d8dd036360c319efc72297b01efcc7ccdf656
Tyler Petersen <tylerpetersen02@gmail.com>
2021-08-15 12:59:17 -0700

    connected postgres and express

--- server/index.js
+++ server/index.js
@@ -3,6 +3,20 @@ const express = require('express');
 const morgan = require('morgan');
 const conInfo = require('../config.js');
 const { Pool, Client } = require('pg');
+const port = 3000;
+// const pg = require('pg');
+
+// class Pool {
+//   _pool = null;
+
+//   connect(options) {
+//     this._pool = new pg.Pool(options);
+//     return this._pool.query('select 1 + 1;');
+//   }
+// }
+
+// const pool = new Pool();
+// pool.connect(conInfo);
 
 dotenv.config();
 const app = express();
@@ -19,7 +33,7 @@ app.use(morgan('dev'));
 const client = new Client(conInfo);
 client.connect()
   .then(() => console.log('Connected!'))
-  .catch(err => console.error(err.stack));
+  .catch(err => console.log(err));
 
 
 // client.query(`select * from reviews where id=2`, (err, res) => {
@@ -36,6 +50,6 @@ app.get('/', (req, res) => {
 });
 
 
-app.listen(conInfo.port, () => {
-  console.log(`Example app listening at http://localhost:${conInfo.port}`);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
